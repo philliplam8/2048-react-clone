a3b0dd8aad4b06de5973ba8b1e0655b04daef141
Phillip <phillip.lam0@gmail.com>
2022-06-26 21:01:50 -0400

    refactor: updated ScoreboardTable component to use useEffect to make AJAX call and use AJAX results to set local state

--- src/components/ScoreboardTable.js
+++ src/components/ScoreboardTable.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState, useEffect } from "react";
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -6,56 +6,82 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+
+const mongoDatabaseEndpoint = 'http://localhost:4000/scoreboard/rankings';
 
 function createData(userScore, userName) {
     return { userScore, userName };
 }
 
-const rows = [];
-
-/// API TO DATABASE ///
-const mongoDatabaseEndpoint = 'http://localhost:4000/scoreboard/rankings';
-async function getapi() {
-    // Store response
-    const response = await fetch(mongoDatabaseEndpoint);
-    // Store data in form of JSON
-    var data = await response.json();
-    // console.log(data);
-    // show(data);
-    for (let i = 0; i < Object.keys(data).length; i++) {
-        rows.push(
-            createData(data[i].score, data[i].name)
-        )
-    }
-}
-getapi(); //TODO THIS AINT THE REACT WAY
-/// API TO DATABASE ///
-
 export default function ScoreboardTable() {
 
-    return (
-        <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 200 }} aria-label="simple table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Score</TableCell>
-                        <TableCell align="right">Name</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows.map((row) => (
-                        <TableRow
-                            key={row.userScore}
-                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                        >
-                            <TableCell component="th" scope="row">
-                                {row.userScore}
-                            </TableCell>
-                            <TableCell align="right">{row.userName}</TableCell>
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [scoreboardEntries, setScoreboardEntries] = useState([]);
+
+    // Make AJAX call and use AJAX results to set local state
+    useEffect(() => {
+        fetch(mongoDatabaseEndpoint)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+
+                    let newItem = [];
+                    for (let i = 0; i < Object.keys(result).length; i++) {
+                        newItem.push(
+                            createData(result[i].score, result[i].name)
+                        )
+                    }
+
+                    setScoreboardEntries(newItem);
+                    console.log(newItem);
+
+                },
+                // Handle errors so that we "don't swallow
+                // exceptions from actual bugs in component"
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+    }, [])
+
+    if (error) {
+        return <div>Error {error.message}</div>;
+    } else if (!isLoaded) {
+        return (
+            <Box sx={{ display: 'flex' }}>
+                <CircularProgress color="inherit" />
+            </Box>
+        );
+    } else {
+        return (
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 200 }} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Score</TableCell>
+                            <TableCell align="right">Name</TableCell>
                         </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
-    );
+                    </TableHead>
+                    <TableBody>
+                        {scoreboardEntries.map((row) => (
+                            <TableRow
+                                key={row.userScore}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            >
+                                <TableCell component="th" scope="row">
+                                    {row.userScore}
+                                </TableCell>
+                                <TableCell align="right">{row.userName}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        );
+    }
 }
